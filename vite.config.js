112faae4b5443d7d4e0aee01ecf8a3a563123539
Aman <aman.hasan@prilient.com>
2026-04-13 17:14:00 +0530

    added base: '/cafes/' in config and scripts in package

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cafes/',
   server: {
     port: 5179,
     host: true,
